97982d0065a521192ba6636fd3b2db93c32a28d1
ltdq <cyzi7777@163.com>
2026-03-18 18:27:40 +0800

    对 main 做对应更改 为了学号索引

--- main.c
+++ main.c
@@ -4,24 +4,34 @@
 
 int main(void) {
   data_init();
-  data_insert("Hello", "World");
-  data_insert("C", "NB");
+  data_insert("Hello", "20250318", "World");
+  data_insert("C", "20260318", "NB");
   data_get("Hello");
+  data_get("20250318");
   data_get("C");
+  data_get("20260318");
   data_save("test.dat");
   data_load("test.dat");
   data_get("Hello");
+  data_get("20250318");
   data_get("C");
-  data_insert("Hello", "Everyone");
+  data_get("20260318");
+  data_insert("Hello", "20250318", "Everyone");
   data_modify("Hello", "Everyone");
   data_get("Hello");
-  data_insert("C", "Programming");
+  data_modify("20250318", "id_test_Hello");
+  data_get("Hello");
+  data_insert("C", "20260318", "Programming");
   data_modify("C", "Programming");
   data_get("C");
-  data_delete("Hello");
-  data_get("Hello");
-  data_delete("C");
+  data_modify("20260318", "id_test_C");
   data_get("C");
+  data_delete("20250318");
+  data_get("Hello");
+  data_get("20250318");
+  data_delete("20260318");
+  data_get("C");
+  data_get("20260318");
   data_exit();
   return 0;
 }
